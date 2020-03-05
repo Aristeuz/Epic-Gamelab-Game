ad54294224031dc5b5ad3081d31704279218f840
Aristeus <jornjellema@hotmail.com>
2020-03-05 23:33:25 +0100

    Almost halfway with the UI remake 1.0

--- Hymn of Lyr/Assets/Scripts/ClickToMove.cs
+++ Hymn of Lyr/Assets/Scripts/ClickToMove.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ClickToMove : MonoBehaviour
 {
@@ -20,7 +21,7 @@ public class ClickToMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             //Locate where the player clicked on the terrain
             locatePosition();
