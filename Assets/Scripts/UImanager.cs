13b1beec065e846c23682205fb57810a5bf8dfa4
Aristeuz <jornjellema@hotmail.com>
2020-03-09 16:22:51 +0100

    Added the abcd stuff

--- Assets/Scripts/UImanager.cs
+++ Assets/Scripts/UImanager.cs
@@ -11,10 +11,12 @@ public class UImanager : MonoBehaviour
     private Button[] actionButtons;
 
     private KeyCode action1, action2, action3, action4;
-
+    public GameObject primeMusic;
 
     void Start()
     {
+        primeMusic = GameObject.Find("Player/PlayerCollider");
+        Debug.Log (primeMusic.name);
         //Keybinds
         action1 = KeyCode.Alpha1;
         action2 = KeyCode.Alpha2;
@@ -28,18 +30,22 @@ public class UImanager : MonoBehaviour
         if (Input.GetKeyDown(action1))
         {
             ActionButtonOnClick(0);
+            primeMusic.GetComponent<PrimeMusicManager>().currentNote = ("a");
         }
         if (Input.GetKeyDown(action2))
         {
             ActionButtonOnClick(1);
+            primeMusic.GetComponent<PrimeMusicManager>().currentNote = ("b");
         }
         if (Input.GetKeyDown(action3))
         {
             ActionButtonOnClick(2);
+            primeMusic.GetComponent<PrimeMusicManager>().currentNote = ("c");
         }
         if (Input.GetKeyDown(action4))
         {
             ActionButtonOnClick(3);
+            primeMusic.GetComponent<PrimeMusicManager>().currentNote = ("d");
         }
     }
 
@@ -47,5 +53,6 @@ public class UImanager : MonoBehaviour
     private void ActionButtonOnClick(int buttonIndex)
     {
         actionButtons[buttonIndex].onClick.Invoke();
+        
     }
 }
