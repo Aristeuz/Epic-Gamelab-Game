64bb8ecc31dceb4d00b01adef70a5e4242f01be3
Aristeus <jornjellema@hotmail.com>
2020-04-10 00:23:30 +0200

    Added a very simple menu by pressing Esc

---
+++ Assets/Scripts/escMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class escMenu : MonoBehaviour
+{
+    public GameObject escapeMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            escapeMenu.gameObject.SetActive(!escapeMenu.gameObject.activeSelf);
+        }
+    }
+}
