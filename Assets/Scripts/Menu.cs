06ac33c103bd3e5cc7c605cd1ca9635ab395d541
LunaLightsss <cheyennekoops@live.nl>
2020-04-21 22:37:26 +0200

    Menu all buttons working
    Whoop Whoop
    Will redesign the settings next

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+   //currently both resume and new game, will go to the demo scene, as there is no save game option yet
+    public void StartGame (){
+        SceneManager.LoadScene("Demo Scene");
+    }
+
+    public void LoadSettings (){
+        SceneManager.LoadScene("Settings");
+    }
+
+   public void ExitGame (){
+       Application.Quit();
+       Debug.Log("Quit the game");
+   }
+
+   public void BackToMenu (){
+       SceneManager.LoadScene("Menu");
+   }
+
+
+}
